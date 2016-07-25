b10db1e1571f72a1aa412cb82e1900ba46ab977b
Richard Hughes <richardh522@gmail.com>
2016-07-25 13:19:24 -0400

    adding start file

--- startOO.py
+++ startOO.py
@@ -2,6 +2,5 @@ import subprocess
 import os
 this_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(this_dir)
-print os.listdir(this_dir)
 p = subprocess.Popen(['soffice', '-headless', '-nofirststartwizard',
                       '-accept=pipe,name=abraxas;urp;StarOffice.Servicemanager'])
